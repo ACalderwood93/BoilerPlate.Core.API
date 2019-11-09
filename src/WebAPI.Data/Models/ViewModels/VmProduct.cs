d83fe9acfba62059b53c15b528cd5560aa7a42ad
Andrew <Andrew.calderwood93@gmail.com>
2019-11-09 15:32:26 +0000

    Added object ID

--- src/WebAPI.Data/Models/ViewModels/VmProduct.cs
+++ src/WebAPI.Data/Models/ViewModels/VmProduct.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
@@ -7,6 +8,9 @@ namespace WebAPI.Data.Models.ViewModels
 {
     public class VmProduct
     {
+
+        public ObjectId _id { get; set; }
+
         [Required]
         public string ProductName { get; set; }
     }
